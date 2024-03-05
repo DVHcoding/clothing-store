131ed5eb7630d02ef18706988005aa8897a487e6
Đỗ Hùng <dohung2ka3@gmail.com>
2024-03-05 11:12:10 +0700

    fix url

--- src/components/Home/ProductCard.js
+++ src/components/Home/ProductCard.js
@@ -22,8 +22,8 @@ const ProductCard = ({ product }) => {
 
 
     return (
-        <Link className="productCard" to={`/product/${product._id}`}>
-            <img src={product.images[0].url} alt={product?.name} />
+        <Link className="productCard" to={`/product/${product?._id}`}>
+            <img src={product?.images[0]?.url} alt={product?.name} />
             <p>{product.name}</p>
 
             <div>
